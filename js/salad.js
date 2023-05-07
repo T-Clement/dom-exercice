44e2215f5bfd6ed35b86faaf1bf1da0e24a229c2
Clément Toquet <toquet.clement@gmail.com>
2023-05-07 17:28:12 +0200

    salad-picker, missing put back ingredient picked in available ingredients on click

--- js/salad.js
+++ js/salad.js
@@ -1,9 +1,11 @@
 
 const ingredients = ["🍅", "🥑", "🥕", "🌽", "🌶️", "🥦", "🥒", "🍄", "🧄", "🧅", "🧀", "🍠", "🥚", "🥬"];
-
 const ingredientsAvailable = document.getElementById("salad-ingredients");
-const ingredientsInSalad = document.getElementById("salad-final");
 console.log(ingredientsAvailable);
+
+
+const ingredientsPicked = [];
+const ingredientsInSalad = document.getElementById("salad-final");
 console.log(ingredientsInSalad);
 
 
@@ -11,17 +13,71 @@ console.log(ingredientsInSalad);
 ingredients.forEach(function(ingredient) {
 
     // create the li and button related
-    let ingredientBtn = document.createElement("li");
+    let li = document.createElement("li");
     let button = document.createElement("button");
     button.classList.add('button');
-    ingredientBtn.appendChild(button);
+    li.appendChild(button);
+    // console.log("Button: " + button)
 
     // add text to button
-    ingredientBtn.textContent = ingredient;
+    button.textContent = ingredient;
 
     // add li to the DOM
-    ingredientsAvailable.appendChild(ingredientBtn);
+    ingredientsAvailable.appendChild(li);
 });
 
-const listItemsAvailable = document.querySelectorAll("#salad-ingredients li")
+const listItemsAvailable = document.querySelectorAll("#salad-ingredients li");
 console.log(listItemsAvailable);
+
+const listItemsAvailableBtn = document.querySelectorAll("#salad-ingredients li .button");
+console.log(listItemsAvailableBtn);
+
+// listItemsAvailableBtn.addEventListener
+
+
+// eventListener, if click -> passed in salad-list
+
+// const ingredientsInSalad = document.getElementById("salad-final");
+ingredientsAvailable.addEventListener("click", function(event) {
+
+    // if click on != button, quit function
+    if (event.target.tagName !== 'BUTTON') return;
+
+
+
+    // block to 5 ingredients
+    if (ingredientsPicked.length >= 5) return;
+
+
+    console.log("EVENT " +event.target.innerText);
+    // console.log(this);
+    console.log(ingredientsInSalad);
+
+
+    event.target.remove();
+
+
+    // add picked indredients to list
+    ingredientsPicked.push(event.target.innerText);
+
+    // create li element
+    let saladItem = document.createElement("li");
+    // create button element
+    let button = document.createElement("button");
+    button.classList.add("button");
+    // add button inside li
+    saladItem.appendChild(button);
+    // button take text content from button who gets clicked
+    button.textContent = event.target.innerText;
+    // add li to ul of picked ingredients
+    ingredientsInSalad.appendChild(saladItem);
+
+
+    console.log(ingredientsPicked);
+});
+
+// handle click on ul !!!!!!!!
+
+ingredientsPicked.addEventListener("click", function() {
+
+})
