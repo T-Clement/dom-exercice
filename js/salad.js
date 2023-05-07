e4470b5951e39f9e4975d59ae22f139e70dd469c
Clément Toquet <toquet.clement@gmail.com>
2023-05-07 15:13:52 +0200

    salad picker, create list of ingredients in DOM

--- js/salad.js
+++ js/salad.js
@@ -1,2 +1,27 @@
 
 const ingredients = ["🍅", "🥑", "🥕", "🌽", "🌶️", "🥦", "🥒", "🍄", "🧄", "🧅", "🧀", "🍠", "🥚", "🥬"];
+
+const ingredientsAvailable = document.getElementById("salad-ingredients");
+const ingredientsInSalad = document.getElementById("salad-final");
+console.log(ingredientsAvailable);
+console.log(ingredientsInSalad);
+
+
+// for each ingredient in js list, add a button to the page
+ingredients.forEach(function(ingredient) {
+
+    // create the li and button related
+    let ingredientBtn = document.createElement("li");
+    let button = document.createElement("button");
+    button.classList.add('button');
+    ingredientBtn.appendChild(button);
+
+    // add text to button
+    ingredientBtn.textContent = ingredient;
+
+    // add li to the DOM
+    ingredientsAvailable.appendChild(ingredientBtn);
+});
+
+const listItemsAvailable = document.querySelectorAll("#salad-ingredients li")
+console.log(listItemsAvailable);
